e568436cb2f40b963c9248c7e2266464292dedfa
Manonrult <manon-ru@live.fr>
2025-09-09 15:51:59 +0200

    Correction marges footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import './sass/Main/main.scss';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Housing from './pages/Housing';
 import ErrorPage from './pages/ErrorPage';
 
-
 function App() {
-  
   return (
-    <div>     
+    <div>
       <Layout>
         {/*routing*/}
         <Routes>
-            <Route path="/" element={<Home />} /> 
-            <Route path="/about" element={<About />} />
-            <Route path="/housing" element={<Housing />} />
-            <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/housing" element={<Housing />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Layout>
     </div>
-  )
+  );
 }
 
 export default App;
