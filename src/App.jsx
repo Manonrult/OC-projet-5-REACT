4f88ae4a8956d99c20288f1a87cfdabedb942d39
Manonrult <manon-ru@live.fr>
2025-09-10 15:48:00 +0200

    MAJ routes + banner responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import './sass/Main/main.scss';
-import { Routes, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-
-import Home from './pages/Home';
-import About from './pages/About';
-import Housing from './pages/Housing';
-import ErrorPage from './pages/ErrorPage';
 
 function App() {
-  return (
-    <div>
-      <Layout>
-        {/*routing*/}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/housing" element={<Housing />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Layout>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import './sass/Main/main.scss';
+
+import Layout from './components/Layout/Layout.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Housing from './pages/Housing.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/housing/:id', //ID pour l'hébergement
+        element: <Housing />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
