2be67f24008dbe5b9438377d63c3218eafc75135
Manonrult <manon-ru@live.fr>
2025-09-10 14:38:09 +0200

    Correction déroulement collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,32 +3,29 @@ import '../sass/Components/Collapse.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
-function Collapse ({title, content}){
-    //ouverture-fermeture composant
-    const [isCollapsed, setIsCollapsed] = useState(true);
+function Collapse({ title, content }) {
+  //ouverture-fermeture composant
+  const [isOpen, setIsOpen] = useState(false);
 
-    //inversion de l'état au clic
-    const toggleCollapse =() => {
-        setIsCollapsed(!isCollapsed);
-    };
+  //inversion de l'état au clic
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
 
-    //Animation de la flèche en fonction de l'état
-    const arrowClass = isCollapsed ? 'collapse__arrow--down' : 'collapse__arrow--up';
-
-    return(
-        <div className='collapse'>
-            <div className='collapse_header' onClick={toggleCollapse}>
-                <h2 className='collapse__title'>{title}</h2>
-                <FontAwesomeIcon icon={faChevronUp} className={arrowClass} />
-            </div>
-                {!isCollapsed && (
-                    <div className='collapse__content'>
-                        {content}
-                    </div>
-                    
-                )}
-        </div>
-    );
+  return (
+    <div className="collapse">
+      <div className="collapse_header" onClick={toggleCollapse}>
+        <h2 className="collapse__title">{title}</h2>
+        <FontAwesomeIcon
+          icon={faChevronUp}
+          className={`collapse__arrow ${isOpen ? 'rotate' : ''}`}
+        />
+      </div>
+      <div className={`collapse__content ${isOpen ? 'open' : ''}`}>
+        <div className="collapse__inner">{content}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Collapse;
